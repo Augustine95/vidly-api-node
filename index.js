25ac3bfcdea1ed3698f6ce8a4281dc3e83e625fe
Augustine Awuori <augutineawuroi95@gmail.com>
2022-03-30 15:42:59 +0300

    Build genre API.

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
+const genres = require('./routes/genres');
 
 mongoose.connect('mongodb://localhost/vidly-api-node')
     .then(() => console.log("Connected to MongoDB..."))
     .catch(err => console.log("Failed to connect to the MongoDB...", err));
 
 app.use(express.json());
+app.use('/api/genres', genres);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
