85d6b605d5c8b6b6ea542afb180f21614087650a
Augustine Awuori <augutineawuroi95@gmail.com>
2022-03-30 16:31:38 +0300

    Build movies API.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 const genres = require('./routes/genres');
+const movies = require('./routes/movies');
 
 mongoose.connect('mongodb://localhost/vidly-api-node')
     .then(() => console.log("Connected to MongoDB..."))
@@ -9,6 +10,7 @@ mongoose.connect('mongodb://localhost/vidly-api-node')
 
 app.use(express.json());
 app.use('/api/genres', genres);
+app.use('/api/movies', movies);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
